04d7b84baf1cf11aa0ff51586f04788809b8c02f
Gabriel Amazonas <gabriel.amazonas.eng@gmail.com>
2018-02-05 09:58:49 -0200

    Creatine User Activity and layout

---
+++ app/src/main/java/com/example/gabrielamazonas/teamme/CreateUserActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gabrielamazonas.teamme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+}
